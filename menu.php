c11d15a42fc97d9c4e9df252ef70ba6eaa0ab241
Magnus Kirø <magnuskiro@gmail.com>
2013-02-27 13:33:19 +0100

    fixed the mun a bit

--- menu.php
+++ menu.php
@@ -1,17 +1,18 @@
 <div class="navbar navbar-inverse" style="position: static;">
               <div class="navbar-inner">
                 <div class="container">
-                  <a class="brand" href="index.php">Hjem</a>
                     <ul class="nav">
+                  	  <li><a href="index.php">Hjem</a></li>
                       <!-- <li class="active"><a href="#">Severdigheter</a></li>-->
                       <li><a href="turist.php">Severdigheter</a></li>
                       <li><a href="restaurant.php">Uteliv</a></li>
                       <li><a href="shopping.php">Shopping</a></li>
-                    </ul>
 
-                    <form class="navbar-search pull-right" action="">
+                    <form class="navbar-search" action="">
                       <input type="text" class="search-query span2" placeholder="Finn destinasjon">
                     </form>
+
+                    </ul>
 
                 </div>
               </div><!-- /navbar-inner -->
