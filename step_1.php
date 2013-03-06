7a6da9a138c2e460fa81a9d37aa3ad7f65df4a71
Dag Erik Vikan <dvikan@gmail.com>
2013-03-06 13:55:44 +0100

    steg 1 av reiseplanlegger
    Viser hvor man skal gå fra nederst i munkegata til dronningens gate

---
+++ step_1.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<?PHP include("head.php"); ?>
+
+<body data-spy="scroll" data-target=".bs-docs-sidebar" data-twttr-rendered="true">
+
+<?PHP include("st.php"); ?>
+<?PHP include("menu.php"); ?>
+
+
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr=Munkegata&amp;daddr=Dronningens+gate&amp;hl=no&amp;geocode=FRLpxwMdFpieAA%3BFRzlxwMd-JeeAA&amp;aq=&amp;sll=63.432504,10.393968&amp;sspn=0.00138,0.005284&amp;dirflg=w&amp;mra=dme&amp;mrsp=0&amp;sz=18&amp;ie=UTF8&amp;t=m&amp;ll=63.432504,10.393968&amp;spn=0.00138,0.005284&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=d&amp;source=embed&amp;saddr=Munkegata&amp;daddr=Dronningens+gate&amp;hl=no&amp;geocode=FRLpxwMdFpieAA%3BFRzlxwMd-JeeAA&amp;aq=&amp;sll=63.432504,10.393968&amp;sspn=0.00138,0.005284&amp;dirflg=w&amp;mra=dme&amp;mrsp=0&amp;sz=18&amp;ie=UTF8&amp;t=m&amp;ll=63.432504,10.393968&amp;spn=0.00138,0.005284" style="color:#0000FF;text-align:left">Vis større kart</a></small>
+
+<?PHP include("menu.php"); ?>
+
+</body>
+</html>
